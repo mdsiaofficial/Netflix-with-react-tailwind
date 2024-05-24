38e9ce1ee971c1761a02878588b8278c238a7b99
rvdashique <rvdashique@gmail.com>
2024-05-24 05:25:37 +0000

    add

--- src/comps/Button.jsx
+++ src/comps/Button.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 
 export default function Button({ Name }) {
   return (
-    <div className="rounded-lg bg-red-700 h-[3rem] w-[8rem] m-5 flex justify-center items-center hover:bg-red-600">
+    <div className="rounded-lg bg-red-700 h-[2rem] w-[5rem] m-5 flex justify-center items-center hover:bg-red-600">
       <button className="flex justify-center items-center w-full h-full">
-        <h1 className="font-bold text-xl hover:text-3xl">{Name}</h1>
+        <h1 className="text-white text-center">{Name}</h1>
       </button>
     </div>
   );
--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import logo from './imgs/logo.png'
+import Button from './Button.jsx'
 
 export default function Header() {
   return (
     <>
       <div className="flex justify-center items-center">
         <img src={logo} alt="" />
+        <div className="flex items-end justify-end">
+          <Button Name={ 'Sign In' }/>
+          <Button Name={ 'Sign Up' }/>
+        </div>
       </div>
     
     </>
